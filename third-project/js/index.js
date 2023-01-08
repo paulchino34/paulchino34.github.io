4f8fb98578f3cf783acaafb745e1b39a23c7707a
paulchino34 <paul-jara@hotmail.com>
2023-01-08 03:15:38 -0500

    Consumo de api
    Consumo de apis y creacion de un array con la data necesaria para generar los graficos

--- third-project/js/index.js
+++ third-project/js/index.js
@@ -1,15 +1,98 @@
-const setData = (data) => console.log(data)
+window.addEventListener('DOMContentLoaded', (event) => {
+    retrievePokemons('https://pokeapi.co/api/v2/pokemon/')
+})
 
-const peticionGet = async (url) => {
-    const respuesta = await fetch(url, {
+let previousPageUrl = null
+let nextPageUrl = null
+let selectedPokemons = []
+let dataPokemons = []
+
+const retrievePokemons = async (url) => {
+    const response = await fetch(url, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         }
     })
 
-    const datos = await respuesta.json()
-    setData(datos.results)
+    const data = await response.json()
+    loadTable(data.results)
+    loadButtons(data)
 }
 
-peticionGet('https://pokeapi.co/api/v2/pokemon/')
+const retrieveDataOfPokemon = async (url) => {
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    const data = await response.json()
+    let element = new Object()
+    element.name = data.name
+    element.height = data.height
+    element.weight = data.weight
+
+    dataPokemons.push(element)
+}
+
+const loadTable = (pokemons) => {
+    let table = document.getElementById('tblAllPokemons')
+    deleteRows(table)
+    if(pokemons.length >= 0) {
+        pokemons.forEach(pokemon => {
+            let rowCount = table.rows.length
+            let row = table.insertRow(rowCount)
+            let checked = selectedPokemons.indexOf(pokemon.url) > 0 ? 'checked' : ''
+            row.insertCell(0).innerHTML = `<input type="checkbox" value="${pokemon.name}" onclick="editList(this, '${pokemon.url}')" ${checked}/>`
+            row.insertCell(1).innerHTML = pokemon.name
+        })
+    }    
+}
+
+const deleteRows = (table) => {
+    var rowCount = table.rows.length;
+    for (var i = rowCount - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }
+}
+
+const loadButtons = (allDataPokemon) => {
+    let btnPrevious = document.getElementById('btnPrevious')
+    let btnNext = document.getElementById('btnNext')
+    btnPrevious.hidden = allDataPokemon.previous ? false : true
+    btnNext.hidden = allDataPokemon.next ? false : true
+
+    previousPageUrl = allDataPokemon.previous
+    nextPageUrl = allDataPokemon.next
+}
+
+const previousPage = () => {
+    retrievePokemons(previousPageUrl)
+}
+
+const nextPage = () => {
+    retrievePokemons(nextPageUrl)
+}
+
+const editList = (element, pokemon) => {
+    if(element.checked) {
+        selectedPokemons.push(pokemon)
+        retrieveDataOfPokemon(pokemon)
+    } else {
+        let index = selectedPokemons.indexOf(pokemon)
+        selectedPokemons.splice(index, 1)
+        removePokemonData(element.value)
+    }
+}
+
+const removePokemonData = (pokemonName) => {
+    dataPokemons.filter((value, index, array) => {
+        if(value.name == pokemonName) {
+            array.splice(index, 1)
+            return true
+        }
+        return false
+    })
+} 
